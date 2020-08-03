ed44ba7f1965003b3020fa780d9a8a406f1e9a9d
zatsaroi <oleg_zoi@yahoo.com>
2020-08-03 14:34:33 -0500

    Aug03

--- DBSR_MAT3_MPI/b-version/br_fake.f90
+++
@@ -1,9 +0,0 @@
-      Subroutine Br_dets
-      End Subroutine Br_dets
-
-      Subroutine Br_buffer
-      End Subroutine Br_buffer
-
-      Subroutine Br_barrier
-      End Subroutine Br_barrier
-      
